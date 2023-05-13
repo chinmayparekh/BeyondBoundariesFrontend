297c8d1b54e492fb9bc27392dd3f373c9bf746a2
Chinmay Parekh <76399431+chinmayparekh@users.noreply.github.com>
2023-05-13 19:25:19 +0530

    added dropdown for selecting the graphs

--- src/components/Predef.js
+++ src/components/Predef.js
@@ -14,8 +14,9 @@ import {
   BarChart,
 } from "recharts";
 import { VictoryPie, VictoryLabel } from "victory";
+import "../styles/Page.css";
 
-const ChartData = () => {
+const ChartData = (props) => {
   const [venue, setVenue] = useState([]);
   useEffect(() => {
     axios
@@ -27,8 +28,13 @@ const ChartData = () => {
         console.log(error);
       });
   }, []);
-  const { x, y, type } = venue;
-
+  const { x, y } = venue;
+  const [type, setSelectedValue] = useState(props.selectedValue);
+  useEffect(() => {
+    setSelectedValue(props.selectedValue);
+  }, [props.selectedValue]);
+  console.log(props)
+  console.log(type);
   const chartData = x?.map((id, index) => ({
     id,
     city: y[index],
@@ -36,51 +42,74 @@ const ChartData = () => {
   console.log(chartData);
   if (type === "line") {
     return (
-      <BarChart width={1200} height={500} data={chartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="id" angle={-20} textAnchor="end" />
-        <YAxis domain={[0, 200]} />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="city" fill="#8884d8" />
-      </BarChart>
+      <div className="centralize mt-50 mb-25">
+        <h1>Line graph for matches played in each city</h1>
+        <BarChart width={800} height={500} data={chartData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="id" angle={-20} textAnchor="end" />
+          <YAxis domain={[0, 200]} />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="city" fill="#8884d8" />
+        </BarChart>
+      </div>
     );
   } else if (type === "area") {
     return (
-      <AreaChart width={500} height={300} data={chartData}>
-        <XAxis dataKey="id" />
-        <YAxis domain={[0, 200]} />
-        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-        <Area type="monotone" dataKey="city" stroke="#8884d8" fill="#8884d8" />
-        <Tooltip />
-        <Legend />
-      </AreaChart>
+      <div className="centralize mt-50 mb-25">
+        <h1>Area graph for matches played in each city</h1>
+        <AreaChart width={800} height={500} data={chartData}>
+          <XAxis dataKey="id" />
+          <YAxis domain={[0, 200]} />
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+          <Area
+            type="monotone"
+            dataKey="city"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+          <Tooltip />
+          <Legend />
+        </AreaChart>
+      </div>
     );
   } else if (type === "pie") {
     const pieData = chartData.map((item) => ({ x: item.id, y: item.city }));
     return (
-      <VictoryPie
-        data={pieData}
-        colorScale="qualitative"
-        radius={100}
-        innerRadius={40}
-        labelRadius={60}
-        style={{ labels: { fill: "black", fontSize: 8, fontWeight: "bold" } }}
-        labelComponent={<VictoryLabel angle={0} />}
-      />
+      <div className="centralize">
+        <h1>Pie graph for matches played in each city</h1>
+        <div className="setHeightWidth">
+          <VictoryPie
+            data={pieData}
+            colorScale="qualitative"
+            radius={100}
+            innerRadius={40}
+            labelRadius={60}
+            style={{
+              labels: { fill: "black", fontSize: 8, fontWeight: "bold" },
+            }}
+            labelComponent={<VictoryLabel angle={0} />}
+            height={400} // set height
+            width={400} // set width
+          />
+        </div>
+      </div>
     );
   } else if (type === "scatter") {
     return (
-      <ScatterChart width={600} height={300}>
-        <CartesianGrid />
-        <XAxis dataKey="id" />
-        <YAxis dataKey="city" domain={[0, 200]} />
-        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-        <Scatter name="Cities" data={chartData} fill="#8884d8" />
-      </ScatterChart>
+      <div className="centralize mt-50 mb-25">
+        <h1>Scatter graph for matches played in each city</h1>
+        <ScatterChart width={800} height={500}>
+          <CartesianGrid />
+          <XAxis dataKey="id" />
+          <YAxis dataKey="city" domain={[0, 200]} />
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Scatter name="Cities" data={chartData} fill="#8884d8" />
+        </ScatterChart>
+      </div>
     );
   } else {
-    return <div>Unsupported chart type</div>;
+    return <div className="centralize">Unsupported chart type</div>;
   }
 };
 
